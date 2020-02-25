56f4671ceed6e4246055cd4a5fe26b5980650771
crespo21 <59964121+crespo21@users.noreply.github.com>
2020-02-25 14:20:10 +0200

    Updated Person.java

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,23 +1,33 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 public class Person {
+     // attributes
     private String name;
     private String gender;
     private int age;
     private String[] Interests;
-
-    Person(String name, String gender, int age, String [] interests){
+    
+    // Overloaded constructor
+    Person(String name, String gender, int age, String [] interests){ 
         this.name = name;
         this.gender = gender;
         this.age = age;
         this.Interests = interests;
-
-
     }
-
+    
     public String Hello(){
-        return "Hello, my name is "+this.name+" and i am "+this.age+" years old. My interests are "
-                +this.Interests[0]+this.Interests[1]+" and "+this.Interests[2];
+        String LastInterest = Interests[Interests.length - 1];
+        ArrayList<String> interests = new ArrayList<String>();
+        for(int i =0; i <Interests.length; i++){
+            if(i < (Interests.length -1))
+                interests.add(Interests[i]);
+        }
+        
+        String ManipInterests =  interests.toString();
+        ManipInterests =ManipInterests.replaceAll("[\\[\\]]","");
+        return  "Hello, my name is "+name+ " and i am "+age+" years old and my interests are "+
+                ManipInterests+" and "+ LastInterest;
     }
-
     public static void main(String[] args){
         Person Ryan = new Person("Ryan","Male",30,new String[] {"being a hardarse ","agile ","ssd hard drives "});
         String greeting =  Ryan.Hello();
